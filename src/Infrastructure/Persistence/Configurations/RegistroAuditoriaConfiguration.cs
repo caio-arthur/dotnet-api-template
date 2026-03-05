beb118e41d9fc26bbad586461e1833a7edccfbc1
Caio Arthur <caioarthur_@hotmail.com>
2026-03-05 14:30:24 -0300

    entity type configurations

---
+++ src/Infrastructure/Persistence/Configurations/RegistroAuditoriaConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class RegistroAuditoriaConfiguration : IEntityTypeConfiguration<RegistroAuditoria>
+    {
+        public void Configure(EntityTypeBuilder<RegistroAuditoria> builder)
+        {
+            builder.HasKey(ra => ra.Id);
+
+        }
+    }
+}
